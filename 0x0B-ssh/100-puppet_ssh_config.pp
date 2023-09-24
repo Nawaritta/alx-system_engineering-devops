0d7bac8a3757657089097a41b4fb9cb976c72b5c
Nawaritta <Nor.benhaddou@gmail.com>
2023-09-24 20:35:04 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #!usr/bin/env bash
 # This script uses puppet to set up our client SSH configuration file so that we can connect to a server without typing a password.
 
-file { 'etc/ssh/ssh_config':
-        ensure => 'present',
-  content =>
-  # SSh configuration
-  Host *
-  PasswordAuthentication no
-  IdentityFile ~/.ssh/school
+file { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  content => "
+  	  # SSH client configuration file
+  	  Host *
+    	  PasswordAuthentication no
+	  IdentityFile ~/.ssh/school
+	  ",
 }
