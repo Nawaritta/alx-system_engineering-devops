eeabd3948f76afdbc6a3a5481d45ca4b7a1a4af3
Nawaritta <Nor.benhaddou@gmail.com>
2023-11-08 20:32:20 +0000

    task done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Corrects the extention typo 'phpp' in 'wp-settings.php' 
+ 
+exec{'fix-wordpress':
+   command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+   path  => 'usr/local/bin/:/bin/'
+}
