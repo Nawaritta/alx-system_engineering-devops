ad6e951216ad42f0b06aa51d50fae456c3f63ce9
Nawaritta <Nor.benhaddou@gmail.com>
2023-11-08 20:39:28 +0000

    puppet-lint fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Corrects the extention typo 'phpp' in 'wp-settings.php' 
- 
+# Corrects the extention typo 'phpp' in 'wp-settings.php'
+
 exec{'fix-wordpress':
-   command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-   path  => 'usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => 'usr/local/bin/:/bin/'
 }
